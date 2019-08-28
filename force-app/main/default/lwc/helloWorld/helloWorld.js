c0162457546b170639eefffbc09e8e80aaa779fa
Ben Barela <benitobarela@yahoo.com>
2019-08-28 10:20:42 -0500

    new new

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,8 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import GenWatt2 from '@salesforce/resourceUrl/GenWatt2';
 import { loadStyle } from 'lightning/platformResourceLoader';
 
 export default class HelloWorld extends LightningElement {
+    @api name = 'World';
     constructor() {
         super();
         loadStyle(this, GenWatt2)
