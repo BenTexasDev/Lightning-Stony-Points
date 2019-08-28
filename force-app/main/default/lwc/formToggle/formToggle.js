15bf16eab43a49c8b01509a6eb275b41c21bdd85
Ben Barela <benitobarela@yahoo.com>
2019-08-28 12:44:18 -0500

    form toggle

--- force-app/main/default/lwc/formToggle/formToggle.js
+++ force-app/main/default/lwc/formToggle/formToggle.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
-export default class FormToggle extends LightningElement {}
+export default class FormToggle extends LightningElement {
+    @track editMode = false;
+    @api recordId;
+
+    handleChange(event){
+        this.editMode = !this.editMode;
+    }
+
+    handleCancel(event){
+        this.editMode = false;
+    }
+
+    handleSave(event){
+        this.editMode = false; // switch back to false so that it will re-render.
+    }
+}
