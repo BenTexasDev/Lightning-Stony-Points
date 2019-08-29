1fafaae19173a31de3a0791075bbbb5e8466f26b
Ben Barela <benitobarela@yahoo.com>
2019-08-29 09:05:40 -0500

    oppCard Push

---
+++ force-app/main/default/lwc/oppCard/oppCard.js
@@ -0,0 +1,34 @@
+import { LightningElement,api,track } from 'lwc';
+import LANG from '@salesforce/i18n/lang';
+import LOCALE from '@salesforce/i18n/locale';
+import CURRENCY from '@salesforce/i18n/currency';
+
+export default class OppCard extends LightningElement {
+    @api name;
+    @api stage;
+    @track formattedDate;
+    @track formattedAmount;
+
+    @api
+    get closeDate(){
+        const dateTimeFormat = new Intl.DateTimeFormat(LANG);
+        this.formattedDate = new Date(this.formattedDate);
+        return dateTimeFormat.format(this.formattedDate);
+    }
+    set closeDate(value){
+        this.formattedDate=value;
+    }
+    @api 
+    get amount() {
+        const numberFormat = new Intl.NumberFormat(LOCALE, {
+            style: 'currency',
+            currency: CURRENCY,
+            currencyDisplay: 'symbol'
+        });    
+        return numberFormat.format(this.formattedAmount);
+    }
+    set amount(value) {
+        this.formattedAmount = value;
+    }
+
+}
---
+++ force-app/main/default/lwc/oppList/oppList.js
@@ -0,0 +1,26 @@
+import { LightningElement, wire,api,track } from 'lwc';
+import getOpportunities from '@salesforce/apex/opportunityController.getOpportunities';
+
+export default class OppList extends LightningElement 
+{
+    @api recordId;
+    @track allOpportunities;
+    @api displayedOpportunities; //captures filter
+    @track error;
+
+    //Start wire
+    @wire(getOpportunities,{accoundId: '$recordId'})
+    wiredOpportunities(value)
+    {
+        this.results = value;
+        if(this.results.data)
+        {//if there is actually data there
+            this.allOpportunities = this.results.data;
+            this.displayedOpportunities = this.results.data;
+        } else if (this.results.error)
+        { //else if there is an error
+            this.error = this.results.error;
+        }
+
+    }
+}
