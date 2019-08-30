c14c902386952a5756599fbf7dba73bfcae4eb57
Ben Barela <benitobarela@yahoo.com>
2019-08-30 08:09:32 -0500

    Fixing the modal

--- force-app/main/default/lwc/oppList/oppList.js
+++ force-app/main/default/lwc/oppList/oppList.js
@@ -1,33 +1,34 @@
-import { LightningElement, wire,api,track } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import getOpportunities from '@salesforce/apex/opportunityController.getOpportunities';
 import getPicklistValues from '@salesforce/apex/opportunityController.getPicklistValues';
+import LOCALE from '@salesforce/i18n/locale';
+import CURRENCY from '@salesforce/i18n/currency';
 import NAME_FIELD from '@salesforce/schema/Opportunity.Name';
 import CLOSE_DATE_FIELD from '@salesforce/schema/Opportunity.CloseDate';
 import AMOUNT_FIELD from '@salesforce/schema/Opportunity.Amount';
 import STAGE_FIELD from '@salesforce/schema/Opportunity.StageName';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import {refreshApex } from '@salesforce/apex';
+import { refreshApex } from '@salesforce/apex';
 import { updateRecord } from 'lightning/uiRecordApi';
 
 
 
-const COLS = [//Added columns here
+const COLS = [
     { label: 'Name', fieldName: 'Name', editable: true },
     { label: 'Stage', fieldName: 'StageName', editable: false },
     { label: 'Amount', fieldName: 'Amount', editable: true, type: 'currency' },
     { label: 'Close Date', fieldName: 'CloseDate', editable: true, type: 'date' }
 ];
 
-export default class OppList extends LightningElement 
-{
+
+export default class OppList extends LightningElement {
     @api recordId;
     @track allOpportunities;
-    @api displayedOpportunities; //captures filter
+    @api displayedOpportunities;
     @track error;
     @api status = 'All';
-    @track dataRetrieved = false;
+    @track dataRetrieved = false; 
     @track iMode = 'Tile';
-
     @api 
     get mode() {
         return this.iMode;
@@ -41,28 +42,36 @@ export default class OppList extends LightningElement
         }
     }
     @track tileMode = true;
-    @track columns = COLS;//Added columns here
+    @track columns = COLS;
     @track formattedAmount = 0;
+    @api 
+    get amount() {
+        const numberFormat = new Intl.NumberFormat(LOCALE, {
+            style: 'currency',
+            currency: CURRENCY,
+            currencyDisplay: 'symbol'
+        });    
+        return numberFormat.format(this.formattedAmount);
+    }
+    set amount(value) {
+        this.formattedAmount = value;
+    }
     @api totalRecords = 0;
-
-    //Start wire
-    @wire(getOpportunities,{accountId:'$recordId'})
-    wiredOpportunities(value)
-    {
+    @wire(getOpportunities, {accountId: '$recordId'})
+    wiredOpportunities(value) {
         this.results = value;
-        if(this.results.data)
-        {//if there is actually data there
+        if (this.results.data) {
             this.allOpportunities = this.results.data;
             this.displayedOpportunities = this.results.data;
-            this.dataRetrieved =true;
-        } else if (this.results.error)
-        { //else if there is an error
+            this.dataRetrieved = true;
+            this.updateList();
+        }
+        else if (this.results.error){
             this.error = this.results.error;
             this.dataRetrieved = false;
         }
-
     }
-    //another wire for picklist values
+
     @wire(getPicklistValues) 
     wiredPicklistValues({ error, data }) {
         if (data) {
@@ -74,71 +83,69 @@ export default class OppList extends LightningElement
             this.error = error;
         }
     }
-    //This is where we add the options to our tile.
-    handleViewChange(event)
-    {
-        const selectedItemValue = event.detail.value;
-        switch(selectedItemValue){
-            case 'tile':
-                this.mode = 'Tile';
-                break;
-            case 'table':
-                this.mode = 'Table';
-                break;
-            default:
-            }
-    }
-    handleChange(event) {
-        this.status = event.detail.value;
-        this.label = event.detail.label;
-        //console.log(this.status);
-        this.updateList();
-    }
 
-    updateList(){
-        var filter = [];
-        var k = 0;
-        var i;
-        var o;
-        this.formattedAmount = 0;
-        if (this.dataRetrieved){
-            for (i=0; i<this.allOpportunities.length;i++){
-                o = this.allOpportunities[i];
-                if(this.status !== 'All'){
-                    if(this.status === 'Open'){
-                        if (!o.IsClosed){
-                            filter[k] = o;
-                            this.formattedAmount += o.Amount;
-                            k++;
-                        }
-                    }else if (this.status === 'Closed') {
-                        if (o.IsClosed) {
-                        //this is Closed,so add it to the filter
-                        filter[k] = o;
-                        this.formattedAmount += o.Amount;
-                        k++;
-                        }
-                    }else if (this.status === o.StageName) {                    
-                     //this is the stage filter
-                        filter[k] = o;
-                        this.formattedAmount += o.Amount;
-                    k++; 
-                    }
-                    }else {
-                        //total the amount
-                        this.formattedAmount += o.Amount;
-                        //add the entire list to the filter
-                        filter = this.allOpportunities;  
-                }
- 
+        handleViewChange(event) {
+            const selectedItemValue = event.detail.value;
+            switch (selectedItemValue) {
+                case 'tile':
+                    this.mode = 'Tile';
+                    break;
+                case 'table':
+                        this.mode = 'Table';
+                    break;
+                default:
             }
-
-            this.displayedOpportunities = filter;
-            this.totalRecords = this.displayedOpportunities.length;//getting the number of elements.
         }
-    }
+        handleChange(event) {
+            this.status = event.detail.value;
+            this.label = event.detail.label;
+            //console.log(this.status);
+            this.updateList();
+        }
+    
+        updateList() { 
+            var filter = [];
+            var k = 0;
+            var i;
+            var o;
+            this.formattedAmount = 0;
+            if (this.dataRetrieved) {
+                for (i=0; i<this.allOpportunities.length; i++){
+                    o = this.allOpportunities[i];
+                    if (this.status !== 'All') {
+                        if (this.status === 'Open') {
+                            if (!o.IsClosed) {
+                              filter[k] = o;
+                              this.formattedAmount += o.Amount;
+                              k++;                              
+                             }
+                            } else if (this.status === 'Closed') {
+                                if (o.IsClosed) {
+                                    //this is Closed,so add it to the filter
+                                    filter[k] = o;
+                                    this.formattedAmount += o.Amount;
+                                    k++;
+                                }
+                            }  else if (this.status === o.StageName) {                    
+                                //this is the stage filter
+                                filter[k] = o;
+                                this.formattedAmount += o.Amount;
+                                k++; 
+                            } 
+                        } else {
+                            //total the amount
+                            this.formattedAmount += o.Amount;
+                            //add the entire list to the filter
+                            filter = this.allOpportunities;  
+                        }
+                    }
+                    this.displayedOpportunities = filter;
+                    this.totalRecords = this.displayedOpportunities.length;
+                }
+            }
 
-    handleSave(event){
+    handleSave(event) {     
+
         const fields = {};
 
         fields['Id'] = event.detail.draftValues[0].Id;
@@ -147,9 +154,9 @@ export default class OppList extends LightningElement
         fields[CLOSE_DATE_FIELD.fieldApiName] = event.detail.draftValues[0].CloseDate;
         fields[STAGE_FIELD.fieldApiName] = event.detail.draftValues[0].StageName;
 
-        const recordInputs = event.detail.draftValues.slice().map(draft => {
-            const fields = Object.assign({},draft);
-            return {fields};
+        const recordInputs =  event.detail.draftValues.slice().map(draft => {
+            const fields = Object.assign({}, draft);
+            return { fields };
         });
 
         console.log('calling promise');
@@ -174,6 +181,5 @@ export default class OppList extends LightningElement
         }).catch(error => {
             // Handle error
         });
-
     }
 }
